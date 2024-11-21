447dfd192ed0f2cf3d38c61612b6f75e10c828aa
michael-otieno <otieno.oduor.michael@gmail.com>
2024-11-21 13:38:43 -0800

    update

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://mike:Qwerty@123@ .wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://mike:Qwerty@123@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://mike:Qwerty@123@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
+
+
+// cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
