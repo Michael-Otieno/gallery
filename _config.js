6ff6c3c2e8740646bd19e396aa86acaed8f488b6
michael-otieno <otieno.oduor.michael@gmail.com>
2024-11-24 00:55:02 -0800

    update

--- _config.js
+++ _config.js
@@ -2,11 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    test: 'mongodb+srv://mike:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    production: 'mongodb+srv://mykey:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://mykey:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://mykey:Qwerty123@cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
 
 
-// cluster0.1djpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
